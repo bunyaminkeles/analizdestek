9224b167f0b21703d74eada548cfefbef69f0c6b
Bunyamin Keles <bkeles74@gmail.com>
2026-01-13 19:48:11 +0100

    update

--- forum/models.py
+++ forum/models.py
@@ -58,3 +58,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.subject}"
