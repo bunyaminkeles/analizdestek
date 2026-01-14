f4194f231984886eb30ab16ec7daf8c2513738d3
Bunyamin Keles <bkeles74@gmail.com>
2026-01-14 20:03:20 +0100

    ADD: Kategori kurulum sistemi

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 # exit on error
 set -o errexit
 
-# 1. Önce Django ve diğer her şeyi yükle
+# 1. Paketleri yükle
 pip install -r requirements.txt
 
 # 2. Statik dosyaları topla
 python manage.py collectstatic --noinput
 
-# 3. Veritabanını güncelle (Burada yapıyoruz ki hata vermesin)
+# 3. Veritabanını güncelle
 python manage.py migrate --noinput
+
+# 4. KATEGORİLERİ KUR (İşte yeni eklediğimiz kısım)
+python manage.py setup_categories
