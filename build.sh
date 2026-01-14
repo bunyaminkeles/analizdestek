d059b7d657f58c89ead9258e5e29ed900efcf0e6
Bunyamin Keles <bkeles74@gmail.com>
2026-01-14 01:53:09 +0100

    sh file

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+# 1. ADIM: ÖNCE PAKETLERİ YÜKLE (Burası en başta olmalı!)
 pip install -r requirements.txt
 
+# 2. ADIM: SONRA DJANGO KOMUTLARINI ÇALIŞTIR
 python manage.py collectstatic --noinput
