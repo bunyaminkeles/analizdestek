6d673f8f00e3f096513358bf035e93006d070f9c
Bunyamin Keles <bkeles74@gmail.com>
2026-01-14 23:56:23 +0100

    500 err upd4

--- forum/context_processors.py
+++ forum/context_processors.py
@@ -1,18 +1,3 @@
-from .models import PrivateMessage
-from django.db import connection
-
 def unread_messages_count(request):
-    data = {'unread_count': 0}
-    if request.user.is_authenticated:
-        try:
-            # SİGORTA: Tablo veritabanında var mı kontrol et
-            with connection.cursor() as cursor:
-                table_names = connection.introspection.table_names(cursor)
-                if PrivateMessage._meta.db_table in table_names:
-                    count = PrivateMessage.objects.filter(receiver=request.user, is_read=False).count()
-                    data['unread_count'] = count
-        except Exception as e:
-            # Hata ne olursa olsun siteyi çökertme
-            print(f"DM Gözcü Hatası: {e}")
-            pass
-    return data
+    # Tablo sorgusunu tamamen kestik, sadece 0 döndürüyoruz.
+    return {'unread_count': 0}
